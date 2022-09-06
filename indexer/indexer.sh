2aa34d363adbdb5822c4c14849fdefafdb74c5b2
Retr0327 <philcoke35@gmail.com>
2022-09-06 19:32:30 +0800

    Update indexer

--- indexer/indexer.sh
+++ indexer/indexer.sh
@@ -2,19 +2,18 @@ function printGreen() {
     printf "\e[0;32m$1\e[0;m\n"
 }
 
-function printRed() {
-    printf "\e[0;31m$1\e[0;m\n"
+function generateIndex() {
+    java -cp "./WEB-INF/lib/*" nl.inl.blacklab.tools.IndexTool $1 $2 ./tei-data/ ./formats/ptt.blf.yaml
 }
-
-service=$1
-
-if ! [[ "$service" =~ ^(create|add)$ ]]; then
-    printRed "Incorrect service: $service"
-    exit 1
-fi
 
 indexDir=./blacklab-data
  
-java -cp "./WEB-INF/lib/*" nl.inl.blacklab.tools.IndexTool $service $indexDir ./tei-data/ ./formats/ptt.blf.yaml
+if [ -d "${indexDir}" ]; then 
+    generateIndex "add" $indexDir
+fi 
+
+if [ ! -d "$Directory" ]; then
+    generateIndex "create" $indexDir
+fi 
 
 printGreen "Blacklab indexes generated: ${indexDir}"
