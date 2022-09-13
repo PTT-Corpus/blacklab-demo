5f0de93a3de72ae53f7dca4a5d0df7642cfb42b5
Retr0327 <philcoke35@gmail.com>
2022-09-14 04:06:02 +0800

    Remove tei data afterwards

--- indexer/indexer.sh
+++ indexer/indexer.sh
@@ -2,19 +2,33 @@ function printGreen() {
     printf "\e[0;32m$1\e[0;m\n"
 }
 
+function printYellow() {
+    printf "\e[0;33m$1\e[0;33m\n"
+}
+
 function generateIndex() {
     java -cp "./WEB-INF/lib/*" nl.inl.blacklab.tools.IndexTool $1 $2 ./tei-data/ ./formats/custom.blf.yaml
 }
 
-indexDir=./blacklab-data
- 
+indexDir=./blacklab-indexes
+teiDir=./tei-data
 
-# if blacklab-data folder has indexes, use `add` command
+
+if ! [ "$(ls -A $teiDir)" ]; then
+    printYellow 'There is no index to parse!'
+    exit 1
+fi
+
+
+# if blacklab-indexes folder has indexes, use `add` command
 if [ "$(ls -A $indexDir)" ]; then
     generateIndex "add" $indexDir
-    printGreen "Blacklab indexes generated: ${indexDir}"
+    printGreen "Blacklab indexes added at ${indexDir}"
 else
-    # if blacklab-data folder is empty, use `create` command
+    # if blacklab-indexes folder is empty, use `create` command
     generateIndex "create" $indexDir
-    printGreen "Blacklab indexes generated: ${indexDir}"
+    printGreen "Blacklab indexes created at ${indexDir}"
 fi
+
+# after indexing, remove the tei data
+/bin/rm -f ./tei-data/*
